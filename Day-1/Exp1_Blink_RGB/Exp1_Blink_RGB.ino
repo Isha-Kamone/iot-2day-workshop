a03243be83f37be87243b6dfad90fb282bd19e28
Kalind Karia <kalind1610@gmail.com>
2022-12-20 17:42:24 +0530

    feat: added exp1 blink rgb code

---
+++ Day-1/Exp1_Blink_RGB/Exp1_Blink_RGB.ino
@@ -0,0 +1,48 @@
+/*
+ * Exp1_Blink_RGB.ino
+ * 
+ * Aim: With the use of RGB LED, blink one color at a time.
+ * 
+ * Target:        eYFi-Mega dev board (https://www.e-yantra.org/products)
+ * Last modified: 20-12-2022
+ * Author:        e-Yantra Team
+ * 
+ * Connections:
+ * Red LED    -> PH3 (OC4A) -> Arduino pin 6
+ * Blue LED   -> PH4 (OC4B) -> Arduino pin 7
+ * Green LED  -> PH5 (OC4C) -> Arduino pin 8
+ * 
+ * Note: All the LEDs are active low.
+ */
+
+// macro definitions
+#define RED_LED   6
+#define BLUE_LED  7
+#define GREEN_LED 8
+
+void setup() {
+  // set RGB LED pins as Output
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+
+  // turn OFF the LED initially
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(BLUE_LED, HIGH);
+}
+
+void loop() {
+  
+  // BLUE color
+  
+  
+  // RED color
+  
+  
+  // GREEN color
+  digitalWrite(GREEN_LED, LOW);
+  delay(1000);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(1000);
+}
